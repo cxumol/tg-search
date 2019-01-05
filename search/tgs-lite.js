c833cd328954ac390cdcdf70ecf7d57609544319
xiangge.lx <xiangge.lx@alipay.com>
2019-01-06 04:02:52 +0800

    performance improvement & remove jquery

--- search/tgs-lite.js
+++ search/tgs-lite.js
@@ -1,91 +1,100 @@
-//$(".loadmsg").load("messages.html .history");
-
-maxPages=1000; // up to messages999.html
+maxPages = 1000; // up to messages999.html
 tgsIndex = {};
-isAllLoaded = false;
+
 foundIndex = {};
 pathPrefix = 'messages'
+THREAD_NUM = 5
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        var error = new Error(response.statusText)
+        error.response = response
+        throw error
+    }
+}
+
+async function loadFile(fileName) {
+    return fetch(fileName).then(checkStatus).then(resp => resp.text()).then(text => {
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = text;
+        return newDiv.getElementsByClassName('message default')
+    })
+}
 
 function loadAll() {
-
-    var loadPageOKcallBack = function(msgDOMlist, pageNum){
-        console.log( msgDOMlist.children('.message.default').length + " messages loaded." );
-        tgsIndex[pageNum] = genIndexMsg(msgDOMlist.children('.message.default'));
-        // delete msgDOMlist;
-      }
-
-    // https://stackoverflow.com/a/7486008 to test if page/file exists for the moment
-    function isPageExists(pageNum) {
-        $.ajax({
-            type: 'HEAD',
-            async: false,
-            url: pathPrefix+pageNum+".html",
-            success: function() {
-                let msgDOMlist = $('<div><\div>').load(pathPrefix+pageNum+".html .history>div", function () {loadPageOKcallBack(msgDOMlist, pageNum)} );
-            },  
-            error: function() {
-                console.log('All pages loaded.');
-                isAllLoaded = true;
+    Array(THREAD_NUM).fill().map((_, t) => {
+        let messageIndexArray = Array(maxPages / THREAD_NUM).fill().map((_, i) => i * THREAD_NUM + t + 1);
+        messageIndexArray.reduce(async (previousValue, currentValue) => {
+            try {
+                await previousValue
+                return loadPage(currentValue)
+            } catch (error) {
+                // hit the end
+                return Promise.reject();
             }
-        });
-    }
-    
-    // For fisrt messages page
-    let msgDOMlist = $('<div><\div>').load(pathPrefix+".html .history>div", function () {loadPageOKcallBack(msgDOMlist, 1)} );
+        }, Promise.resolve())
+    })
 
-    // For messages2+ .html
-    // for (let pageNum = 2; pageNum < maxPages; pageNum++) {
-    //     isPageExists(pageNum);
-    //     if ( isAllLoaded == true) break;
-    // }
 
-    let pageNum = 2;
-    while (pageNum<=maxPages) {
-        pageNum += 1;
-        isPageExists(pageNum);
-        if (isAllLoaded) break;
+    async function loadPage(pageNum) {
+        let fileName = pageNum === 1 ? pathPrefix + '.html' : pathPrefix + pageNum + '.html';
+        return loadFile(fileName).then(msgList => getIndexMessage(msgList))
+            .then(x => {
+                tgsIndex[pageNum] = x;
+            })
     }
 }
 
 
+function getIndexMessage(msgDivs) {
+    var res = [];
+    for (let msg of msgDivs) {
+        res.push({
+            id: msg.getAttribute('id'),
+            ...extractMessageDate(msg)
+        })
+    }
+    return res;
+}
 
 
+function extractMessageDate(msgDiv) {
+    let bodyDiv = msgDiv.getElementsByClassName('body')[0];
+    let res = {
+        date: bodyDiv.getElementsByClassName('date')[0].getAttribute('title'),
+        from: getInnerText(bodyDiv.getElementsByClassName('from_name')[0]),
+        txt: getInnerText(bodyDiv.getElementsByClassName('text')[0]),
+    }
+    return res
+}
 
-function genIndexMsg(msgElms) {
-    //if ( msgElms === void 0 ) msgElms = '';
-    var elms = [];
-    msgElms.each(function () {
-        elms.push({
-            id: $(this).attr('id'),
-            date: $(this).children('.body').children('.date').attr('title'),
-            from: $(this).children('.body').children('.from_name').text().trim(),
-            txt: $(this).children('.body').children('.text').text().trim()
-        });
-    });
-    return elms
-};
+function getInnerText(x) {
+    return x ? x.innerText.trim() : '';
+}
 
 
 // ref https://github.com/docsifyjs/docsify/blob/master/lib/plugins/search.js search()
-function search(query) { 
+function search(query) {
     query = query.trim();
     var keywords = query.split(/[\s\-，\\/]+/);
     if (keywords.length !== 1) {
         keywords = [].concat(query, keywords);
     }
-    var allMatchMsg={};
-    function findFrom(idx){
+    var allMatchMsg = {};
+    function findFrom(idx) {
         var pageContent = tgsIndex[idx];
         var pageMatchMsg = [];
         keywords.forEach(kw => {
             // From https://github.com/sindresorhus/escape-string-regexp
             var regEx = new RegExp(kw.replace(/[|\\{}()[\]^$+*?.]/g,
-            '\\$&'),'gi'); 
-            
+                '\\$&'), 'gi');
+
 
             // ref https://github.com/ppoffice/hexo-theme-icarus/blob/master/source/js/insight.js weight()
             var pattern = new RegExp(kw, 'img'); // Global, Multi-line, Case-insensitive
-            pageContent.forEach( msg => {
+            pageContent.forEach(msg => {
                 var matches = msg['txt'].match(kw); // return ["keywords1"]
                 if (matches) pageMatchMsg.push(msg);
             })
@@ -103,95 +112,78 @@ function search(query) {
 
 
 function pageShowResults() {
-    var query = arguments.length ? arguments[0] : $input.val();
-    $resultlist.empty();
-    if (query.length==0) return;
+    var query = arguments.length ? arguments[0] : searchBar.value;
+    resultList.innerHTML = ''//todo: maybe we could diff the dom, and do not rerender each time
+    if (query.length == 0) return;
     var aSearch = search(query);
     for (let i in aSearch) {
         aSearch[i].forEach(SResultEntry => {
-            let linkto = i>1? pathPrefix+i+".html#"+SResultEntry.id : pathPrefix+".html#"+SResultEntry.id;
-            $resultlist.append([`<div class= "default message" data-url="`, linkto,`"><div class="from_name">`,
-            SResultEntry.from ,`</div><div class="text">`, SResultEntry.txt, `</div></div>`].join(''));
+            let linkto = i > 1 ? pathPrefix + i + ".html#" + SResultEntry.id : pathPrefix + ".html#" + SResultEntry.id;
+            var child = document.createElement('div');
+            child.innerHTML = `<div class= "default message" data-url="${linkto}"><div class="from_name">${SResultEntry.from}</div><div class="text">${SResultEntry.txt}</div></div>`;
+
+            resultList.appendChild(child.firstChild);
         });
     }
     return aSearch;
 }
 
-$input = $('#tg-searchbar')
-$resultlist = $('.results-list')
 
-function searchBar() {
-    $input.on('input', function () {
-        var keywords = $(this).val();
-        foundIndex = pageShowResults(keywords);
-    });
-    $input.trigger('input');
-}
-
-function previewLoader ($item) {
-    if ($item && $item.length) {
-        let dataUrl = $item.attr('data-url').split('#');
-        let filename = dataUrl[0];
-        let msgShowId = dataUrl[1];
-        let $pageLoad = $('<div><\div>').load(filename + " .history>div", displayMsg);
-
-        console.log(msgShowId+" selected!");
-
-        // https://stackoverflow.com/a/32102391
-        function displayMsg() {
-
-            let chosenOne = $pageLoad.children("#"+dataUrl[1]);
-
-            // I didn't find how to select previous (one or two) elements by CSSselector/jQuerySelector
-            
-            // https://stackoverflow.com/a/12111784
-            let $previewColection=$().add(chosenOne.prev().prev()
-            ).add(chosenOne.prev()
-            ).add(chosenOne.addClass("bold selected")
-            ).add(chosenOne.next()
-            ).add(chosenOne.next().next()
-            ).add(`<a class="pagination block_link" href="`+$item.attr('data-url')+`"> <i class="fas fa-hand-point-right"></i> 回原文定位 </a>`
-            );
-
-            $(".loadmsg").empty(); // clear old previews
-
-            $(".loadmsg").append($previewColection);
-
-            // $(".loadmsg").load(filename + " .history>div #"+dataUrl[1]+" + div,.history>div #"+dataUrl[1]+" + div + div");
-
-            console.log("preview loaded.")
-        }
-        
-    }
-}
-
-function resultPreviewer() {
-    $('.results-list').on('click touchend', '.message', function (e) {
-
-        $('.results-list').find('.selected').removeClass('selected'); // clear old selected mark
-
-        // if (e.type !== 'click' && !touch) {
-        //     return;
-        // }
-
-        $(this).addClass('selected'); // setup new selected mark
-        previewLoader($(this));
-
-        touch = false;
+function initSearchBarTrigger() {
+    searchBar = document.getElementById('tg-searchbar')
+    searchBar.addEventListener('input', () => {
+        foundIndex = pageShowResults(searchBar.value);
     })
 }
 
+
+function previewLoader(item) {
+
+    let dataUrl = item.getAttribute('data-url').split('#');
+    let filename = dataUrl[0];
+    let msgShowId = dataUrl[1];
+    loadFile(filename).then(msgList => displayMessage(msgList, dataUrl, msgShowId))
+
+
+    function displayMessage(msgList, dataUrl, messageId) {
+        let index = Array.prototype.findIndex.call(msgList, (x) => x.id == messageId)
+        msgList[index].classList.add('bold')
+        msgList[index].classList.add('selected')
+        showItems = Array.prototype.slice.call(msgList, index - 3 < 0 ? 0 : index - 3, index + 5)
+        const documentFragment = document.createDocumentFragment();
+        showItems.forEach(c => documentFragment.appendChild(c));
+        let jumpButton = document.createRange().createContextualFragment(`<a class="pagination block_link" href="${dataUrl}"> <i class="fas fa-hand-point-right"></i> 回原文定位 </a>`);
+        documentFragment.appendChild(jumpButton)
+        document.getElementById('loadmsg').innerHTML = ''
+        document.getElementById('loadmsg').appendChild(documentFragment);
+    }
+
+}
+
+
+
+
+function initResultPreviewer() {
+    resultList.addEventListener('click', (event) => {
+        let selectedItem = resultList.getElementsByClassName('selected')
+        if (selectedItem.length > 0) {
+            selectedItem[0].classList.remove('selected')
+        }
+
+
+        for (let e of event.path) {
+            if (e.classList.contains('message')) {
+                e.classList.add('selected');
+                previewLoader(e)
+                break;
+            }
+        }
+    })
+
+}
+
+
+resultList = document.getElementById('results-list')
+initResultPreviewer()
+initSearchBarTrigger()
 loadAll();
-searchBar();
-resultPreviewer();
-
-
-
-
-
-
-
-// pageShowResults("群");
-
-// var testSearch = search("群");
-
